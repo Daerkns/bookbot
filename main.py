7fe4ac8c100523337cd41e2009a45b5829fc435b
Daerkns <daerkns@gmail.com>
2026-02-14 13:03:21 +0600

    bookbot stuff

---
+++ main.py
@@ -0,0 +1,16 @@
+# print("greetings boots")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
